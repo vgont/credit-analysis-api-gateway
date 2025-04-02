1c5cab20f5a4a455610f6d0b15ab356fb897c7c2
vgont <vgontdev@gmail.com>
2025-04-02 14:27:03 -0300

    feat (app.module): kafka client

---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+
+@Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'KAFKA-SERVICE',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            clientId: 'gateway',
+            brokers: ['localhost:9092'],
+          },
+          consumer: {
+            groupId: 'gateway',
+          },
+        },
+      },
+    ]),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
