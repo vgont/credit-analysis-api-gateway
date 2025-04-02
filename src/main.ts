b2afaa930a5d69f0650546dd0079a143a4602c6f
vgont <vgontdev@gmail.com>
2025-04-02 14:25:12 -0300

    feat: app init log

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap().then(() => {
+  Logger.log('🚀 Application is running on port 3000');
+});
