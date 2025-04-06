693cef23ef9fb3c64dc36c03058450ce33b92f04
vgont <vgontdev@gmail.com>
2025-04-06 19:13:28 -0300

    feat: invoice routes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,6 +2,7 @@ import {
   Body,
   Controller,
   Get,
+  Headers,
   Param,
   Post,
   Res,
@@ -15,6 +16,11 @@ import {
 import { Response } from 'express';
 import { ZodValidationPipe } from './pipes/zod-validation.pipe';
 import { getAllCreditAnalysisSchema } from './dto/getAllCreditAnalysis.dto';
+import {
+  RegisterClientDto,
+  registerClientSchema,
+} from './dto/registerClient.dto';
+import { UploadInvoiceDto, UploadInvoiceSchema } from './dto/uploadInvoice.dto';
 
 @Controller()
 export class AppController {
@@ -23,6 +29,12 @@ export class AppController {
   @Get('/health')
   getHello(@Res() res: Response): Response {
     return res.send(this.appService.healthCheck());
+  }
+
+  @Post('register-client')
+  @UsePipes(new ZodValidationPipe(registerClientSchema))
+  registerClient(@Body() registerClientDto: RegisterClientDto) {
+    this.appService.registerClient(registerClientDto);
   }
 
   @Post('create-credit-analysis')
@@ -50,6 +62,52 @@ export class AppController {
     } catch (error) {
       console.log(error);
       return res.status(500).send('Internal server error');
+    }
+  }
+
+  @Post('invoice')
+  @UsePipes(new ZodValidationPipe(UploadInvoiceSchema))
+  uploadInvoice(
+    @Body() invoice: UploadInvoiceDto,
+    @Headers() headers: { [key: string]: string },
+  ) {
+    const clientEmail = headers['client-email'];
+    try {
+      this.appService.uploadInvoice(invoice, clientEmail);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  @Get('invoice/:invoiceNumber')
+  async getInvoice(
+    @Param('invoiceNumber') invoiceNumber: string,
+    @Headers() headers: { [key: string]: string },
+  ): Promise<any> {
+    try {
+      const clientEmail = headers['client-email'];
+      const invoice = await this.appService.getInvoice(
+        invoiceNumber,
+        clientEmail,
+      );
+      return invoice;
+    } catch (error) {
+      console.log(error);
+      return 'No invoice found';
+    }
+  }
+
+  @Get('invoices')
+  async getAllInvoices(
+    @Headers() headers: { [key: string]: string },
+  ): Promise<any> {
+    try {
+      const clientEmail = headers['client-email'];
+      const invoices = await this.appService.getAllInvoices(clientEmail);
+      return invoices;
+    } catch (error) {
+      console.log(error);
+      return 'No invoices found';
     }
   }
 }
