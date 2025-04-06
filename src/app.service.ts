224b8f265d3574e5be4c78a2035b8d801201ff6b
vgont <vgontdev@gmail.com>
2025-04-06 19:13:44 -0300

    feat: invoice messages

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,8 @@ import { ClientKafka } from '@nestjs/microservices';
 import { CreateAnalysisDto } from './dto/createAnalysis.dto';
 import { firstValueFrom } from 'rxjs';
 import { ICreditAnalysis } from './app.interfaces';
+import { RegisterClientDto } from './dto/registerClient.dto';
+import { UploadInvoiceDto } from './dto/uploadInvoice.dto';
 
 @Injectable()
 export class AppService {
@@ -12,11 +14,17 @@ export class AppService {
 
   async onModuleInit() {
     this.kafkaClient.subscribeToResponseOf('getAllCreditAnalysis');
+    this.kafkaClient.subscribeToResponseOf('getInvoice');
+    this.kafkaClient.subscribeToResponseOf('getAllInvoices');
     await this.kafkaClient.connect();
   }
 
+  registerClient(registerClientDto: RegisterClientDto) {
+    this.kafkaClient.emit('registerClient', registerClientDto);
+  }
+
   createCreditAnalysis(createAnalysisDto: CreateAnalysisDto) {
-    this.kafkaClient.emit('createCreditAnalysis', createAnalysisDto);
+    this.kafkaClient.emit('createCreditAnalysis', { createAnalysisDto });
   }
 
   async getAllCreditAnalysis(clientEmail: string): Promise<ICreditAnalysis[]> {
@@ -28,6 +36,20 @@ export class AppService {
       console.log(error);
       return [];
     }
+  }
+
+  uploadInvoice(invoice: UploadInvoiceDto, clientEmail: string) {
+    this.kafkaClient.emit('uploadInvoice', { invoice, clientEmail });
+  }
+
+  async getInvoice(invoiceNumber: string, clientEmail: string): Promise<any> {
+    return firstValueFrom(
+      this.kafkaClient.send('getInvoice', { invoiceNumber, clientEmail }),
+    );
+  }
+
+  async getAllInvoices(clientEmail: string): Promise<any> {
+    return firstValueFrom(this.kafkaClient.send('getAllInvoices', clientEmail));
   }
 
   healthCheck(): { status: string } {
