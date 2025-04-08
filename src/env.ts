5568577c69a0fdae2c768d8c7631b1440d36aed5
vgont <vgontdev@gmail.com>
2025-04-08 02:03:03 -0300

    feat: env validator

---
+++ src/env.ts
@@ -0,0 +1,12 @@
+import { z } from 'zod';
+import 'dotenv/config';
+
+export const envSchema = z.object({
+  DATABASE_HOST: z.string(),
+  DATABASE_PORT: z.coerce.number(),
+  DATABASE_USERNAME: z.string(),
+  DATABASE_PASSWORD: z.string(),
+  DATABASE_NAME: z.string(),
+});
+
+export const env = envSchema.parse(process.env);
